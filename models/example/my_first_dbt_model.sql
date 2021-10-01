74feb81ac90b698314131558637991be791f19ca
crozanski@financeit.io <crozanski@financeit.io>
2021-10-01 17:25:07 +0000

    1st_dbt_model_fix

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,4 +1,3 @@
-
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
@@ -7,7 +6,7 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
+
 
 with source_data as (
 
@@ -20,8 +19,12 @@ with source_data as (
 select *
 from source_data
 
+
+
 /*
     Uncomment the line below to remove records with null `id` values
 */
 
 -- where id is not null
+limit 500
+/* limit added automatically by dbt cloud */
